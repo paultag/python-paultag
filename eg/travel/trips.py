0ce1188157a84972194ec093de88cb508503c8e8
Paul Tagliamonte <tag@pault.ag>
2014-10-21 23:02:55 -0400

    update

--- eg/travel/trips.py
+++ eg/travel/trips.py
@@ -1,5 +1,14 @@
 from paultag.travel.service import TravelService
 
+def print_trip(trip):
+    print("""Trip ID: {id}
+From:    {start}
+Until:   {end}
+
+{reason}
+    """.format(**trip))
+
+
 travel = TravelService()
 for trip in travel.trips("tag"):
-    print(trip)
+    print_trip(trip)
