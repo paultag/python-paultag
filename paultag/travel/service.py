3f680b5069e5e60d9eb1900934138ad1246e421d
Paul Tagliamonte <tag@pault.ag>
2014-10-26 21:14:40 -0400

    Fiddle with the service.

--- paultag/travel/service.py
+++ paultag/travel/service.py
@@ -6,7 +6,7 @@ class TravelService(Service):
         self.setup(host="http://localhost:8000", apikey=None)
 
     def trips(self, who):
-        return self._query("GET", "trips", who)['active_trips']
+        return self._query("GET", "trips", who)
 
     def trip(self, trip_id):
         return self._query("GET", "trip", trip_id)
